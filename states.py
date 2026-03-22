3f645f25d3f4f24a5619ad988e168dcc01176b54
PalazhchenkoDanylo <14.amigo.0@gmail.com>
2026-03-22 18:52:51 +0100

    Add state management for task addition and editing

---
+++ states.py
@@ -0,0 +1,16 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class AddTask(StatesGroup):
+    subject = State()
+    title = State()
+    description = State()
+    deadline = State()
+
+
+class EditTask(StatesGroup):
+    task_id = State()
+    subject = State()
+    title = State()
+    description = State()
+    deadline = State()
